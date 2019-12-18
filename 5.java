21c4098364fbaeca9884c27a8db41ee17783f0cd
SAI <saiteja12dec@gmail.com>
2019-12-18 18:04:56 +0530

    adding 8.java

--- 5.java
+++ 5.java
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 Idea 1
 Dummy Line
+=======
+Idea 2
+>>>>>>> parent of 8845db3... trying stsh pop command
 =======
 Idea 2
 >>>>>>> parent of 8845db3... trying stsh pop command
